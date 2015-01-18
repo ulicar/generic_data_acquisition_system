1c999e1684e2a5a863cb21ebc75236b41e51e824
Josip Domsic <josip.domsic@gmail.com>
2015-01-18 16:28:14 +0100

    *  working copy

--- GDAS/pidgeon/message_selector.py
+++ GDAS/pidgeon/message_selector.py
@@ -2,7 +2,9 @@ __author__ = 'jdomsic'
 
 import sys
 import ConfigParser
-sys.path.append("..")
+import pika
+
+from config import Configuration
 
 from utils.input.argument_parser import get_arguments
 
@@ -15,8 +17,15 @@ def main():
         config = ConfigParser.ConfigParser()
         config.read(ini_file)
 
-        # service, connect to MQ
-        print "OK"
+        cfg = Configuration().load_from_file(ini_file)
+
+        mq = pika.BlockingConnection(pika.ConnectionParameters(host=cfg.msg_queue_url))
+        mq_channel = mq.channel()
+        mq_channel.queue_declare(queue='hello')
+        mq_channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+
+        print " [x] Sent 'Hello World!'"
+        mq.close()
 
     except Exception, e:
         print >>sys.stderr, str(e)
--- GDAS/worker/message_processor.py
+++ GDAS/worker/message_processor.py
@@ -1,7 +1,8 @@
 __author__ = 'jdomsic'
 
 import sys
-import ConfigParser
+import pika
+from config import Configuration
 
 from utils.input.argument_parser import get_arguments
 
@@ -11,11 +12,22 @@ def main():
     ini_file = args.ini
 
     try:
-        config = ConfigParser.ConfigParser()
-        config.read(ini_file)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+        channel = connection.channel()
 
-        # service, connect to MQ
-        print "OK"
+        channel.queue_declare(queue='hello')
+
+        print ' [*] Waiting for messages. To exit press CTRL+C'
+
+        def callback(ch, method, properties, body):
+            print " [x] Received %r" % (body,)
+
+        channel.basic_consume(callback,
+                              queue='hello',
+                              no_ack=True)
+
+        channel.start_consuming()
 
     except Exception, e:
         print >>sys.stderr, str(e)
