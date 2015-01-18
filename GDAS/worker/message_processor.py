d0839bbdba1c295cf482e6ea72991ab01e6fbbe7
Josip Domsic <josip.domsic@gmail.com>
2015-01-18 15:13:57 +0100

    refactor, initial model/skeleton

---
+++ GDAS/worker/message_processor.py
@@ -0,0 +1,27 @@
+__author__ = 'jdomsic'
+
+import sys
+import ConfigParser
+
+from utils.input.argument_parser import get_arguments
+
+
+def main():
+    args = get_arguments()
+    ini_file = args.ini
+
+    try:
+        config = ConfigParser.ConfigParser()
+        config.read(ini_file)
+
+        # service, connect to MQ
+        print "OK"
+
+    except Exception, e:
+        print >>sys.stderr, str(e)
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
