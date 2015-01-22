a52430ece055b2d9b6203f24ecab6a2b85b8652e
Josip Domsic <josip.domsic@gmail.com>
2015-01-22 02:39:05 +0100

    * test helper functions added

---
+++ GDAS/tests/create_core_msg.py
@@ -0,0 +1,60 @@
+__author__ = 'jdomsic'
+
+import json
+import random
+import string
+import datetime
+
+class AggregatorMessage():
+    def __init__(self, dbID, name, ip, port, ports_path, ports_status, res_path, res_status, unique_id, time, err):
+        self._id = dbID
+        self.name = name
+        self.ip = ip
+        self.port = port
+        self.ports = {'path': ports_path, 'status': ports_status}
+        self.resources = {'path': res_path, 'status': res_status}
+        self.id = unique_id
+        self.timeout = time
+        self.error = err
+
+    def validate(self):
+        assert type(self.id) is str and self.id != ''
+
+class CoreMessage():
+    def __init__(self, unique_id, module_type, value, timestamp, dbID=''):
+        self.id = unique_id
+        self._id = dbID
+        self.module = module_type
+        self.value = value
+        self.timestamp = timestamp
+
+    def validate(self):
+        assert type(self.id) is str and self.id != ''
+
+    def get(self):
+        return self.__dict__
+
+
+def create_random_string(size):
+    choices = string.ascii_letters
+    return ''.join(random.choice(choices) for _ in range(size))
+
+def create_random_value(min, max):
+    return random.randint(min, max)
+
+def create_timestamp():
+    return str(datetime.datetime.utcnow())
+
+
+def create_random_core_message():
+    rand_id = create_random_string(10)
+    sensor_value = create_random_value(-10, 50)
+    module_type = 'temperature'
+    ts = create_timestamp()
+
+    return CoreMessage(rand_id, module_type, sensor_value, ts).get()
+
+
+if __name__ == '__main__':
+    for _ in range(1,11):
+        print create_random_core_message()
