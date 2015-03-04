bf20890dc37436687a8624032abf20ddeba249c6
Josip Domsic <josip.domsic@gmail.com>
2015-03-04 20:09:24 +0100

    *moved

--- GDAS/util/auth/auth.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'jdomsic'
-
-class Auth(object):
-    def __init__(self, request):
-        self.username = getattr(request, 'username', None)
-        self.password = getattr(request, 'password', None)
