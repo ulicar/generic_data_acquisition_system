6f8ab57ece2010e452972db2444b50b9c846040f
Josip Domsic <josip.domsic@gmail.com>
2015-03-04 20:49:01 +0100

    * updated Fatty

--- GDAS/util/database/connection.py
+++ GDAS/util/database/connection.py
@@ -6,12 +6,18 @@ from pymongo import Connection
 class Fatty(object):
     def __init__(self, db):
         self.host, self.port = db
+        self.collection = None
 
-    def open(self):
-        return Connection(self.host, self.port)
+    def open(self, collection):
+        self.collection = Connection(self.host, self.port)[collection]
 
-    def read(self):
-        pass
+    def close(self):
+        self.collection = None
 
-    def write(self):
-        pass
+    def read(self, query):
+        data = self.collection.find(query)
+
+        return data
+
+    def write(self, data):
+        self.collection.insert(data) #TODO make keys
