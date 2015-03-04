f9baa4d8fc6cea2cfe7ae2866b0c67e34cf222aa
Josip Domsic <josip.domsic@gmail.com>
2015-03-04 19:56:57 +0100

    * removed deleted funcition call

--- GDAS/tests/wizard/test_response.py
+++ GDAS/tests/wizard/test_response.py
@@ -1,11 +1,9 @@
 __author__ = 'jdomsic'
 
 import unittest
-from wizard.message_collector import check_format
 
 
-class wizard_unit_test(unittest.TestCase):
+class WizardUnittest(unittest.TestCase):
 
     def test_check_format__good_format(self):
         data = {}
-        check_format()
