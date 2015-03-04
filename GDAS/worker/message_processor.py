fcb7f3ee5a837d59ed3c1f787b3f614309ea52fd
Josip Domsic <josip.domsic@gmail.com>
2015-03-04 20:31:00 +0100

    * refactor of Worker (message processor)

--- GDAS/worker/message_processor.py
+++ GDAS/worker/message_processor.py
@@ -20,28 +20,21 @@ class MessageProcessor(object):
         self.loq = cfg.log_file
 
     def main(self):
-        settings = Settings('amqp://guest:guest@localhost:5672/%2F',
-                'example-queue'
-        )
-
+        settings = Settings(self.mq, self.queue_name)
         consumer = Consumer(settings)
+        consumer.consume(self.process_message)
 
-        def callback(message, message_type, properties):
+
+    def process_message(self, message, message_type, properties):
             print str(message)
             # OPTIONAL 1: consumer.acknowledge_msg()
             # OPTIONAL 2: consumer.reject_msg()
+            self.save_to_database()
 
-        consumer.consume(callback)
-
-    def open_mq_channel(self, mq_url):
-        mq = pika.BlockingConnection(pika.ConnectionParameters(host=mq_url))
-        mq_channel = mq.channel()
-        return mq, mq_channel
-
-
+    """
     def process_message(self, ch, method, properties, body):
         save_to_database(body, Connection('localhost', 27017), 'sensor_data', 'weather_data')
-
+    """
 
     def save_to_database(self, message, client, db_name, db_collection):
         database = client[db_name]
@@ -55,11 +48,7 @@ def main():
     cfg = Configuration().load_from_file(args.ini)
 
     message_processor = MessageProcessor(cfg)
-
-    mq, mq_channel = open_mq_channel(cfg.mq_url)
-    mq_channel.basic_consume(process_message, queue=cfg.msg_queue, no_ack=True)
-    mq_channel.start_consuming()
-
+    message_processor.main()
 
 if __name__ == '__main__':
     try:
