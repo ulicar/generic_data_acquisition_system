dc8c27daa0bcc3c15f03eb6be07de74a6812d859
Josip Domsic <josip.domsic@gmail.com>
2015-04-12 21:00:38 +0200

    * removed unnecessary files

--- GDAS/interfaces/compile_protobuf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for proto in $(find . -iname \*.proto); do
-	dir=$(dirname $proto)
-	echo $dir $proto
-	protoc -I/usr/include -I. --python_out=. $proto || exit 1
-done
-
-exit 0
--- GDAS/interfaces/contracts/coding.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'jdomsic'
-
-""" Encode and decode to protobuff message """
