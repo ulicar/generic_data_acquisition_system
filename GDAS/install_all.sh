ddac5bafd89e95b72e7772717813db7d56de5739
Josip Domsic <josip.domsic@gmail.com>
2015-04-28 01:01:12 +0200

    *  python version

--- GDAS/install_all.sh
+++ GDAS/install_all.sh
@@ -21,20 +21,21 @@ PACKAGES={$UTIL $HOBBIT $WORKER $WIZARD $PIGEON}
 [ -d $WORKER ] || echo "$WORKER directory missing" || exit -1
 
 if [ ! -f $PYTHON ]; then
-    echo 'sudo apt-get install python2.6' && exit -1
+    echo 'sudo apt-get install python' && exit -1
 fi
 
 # Test prerequisites
 python -c 'import setuptools' 2>/dev/null
-[ $? == 1 ] && echo '[REQUIRED] Install python2.6 setuptools' && exit -1
+[ $? == 1 ] && echo '[REQUIRED] Install python2.7 setuptools' && exit -1
 
 # Install subsystems
 echo -e 'Starting installation...\n'
-#Creating Users
+
+#Creating User
 for package in ${array[*]}
 do
-    adduser ${package} -g gdas
-    echo -e "Created ${package} user.\n"
+    adduser gdas -g gdas
+    echo -e "Created gdas user.\n"
 done
 
 # Install packages
