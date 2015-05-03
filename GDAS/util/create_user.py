9578a9957dbc868711c95b3d3a4d04a4fd632ef5
Josip Domsic <josip.domsic@gmail.com>
2015-05-03 20:35:32 +0200

    *  modified script: create_user

--- GDAS/util/create_user.py
+++ GDAS/util/create_user.py
@@ -7,6 +7,9 @@ import sys
 from GDAS.utils.database.connection import Fatty
 from GDAS.utils.security.auth import UserAuth
 
+COLLECTION = 'gdas-accounts'
+DATABASE = 'localhost:27017'
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Create users for GDAS')
@@ -14,27 +17,24 @@ def get_arguments():
     parser.add_argument(
         'user_name',
         metavar='USERNAME',
-        required=True,
         help='Username for new user'
     )
 
     parser.add_argument(
         'password',
         metavar='PASSWORD',
-        required=True,
         help='Password for new user'
     )
 
     parser.add_argument(
         'user-descr',
         metavar='USERDECSRIPTION',
-        required=True,
         help='Description for new user'
     )
 
     parser.add_argument(
         '--user-roles',
-        metavar='ROLES',
+        metavar='ROLE',
         nargs='*',
         default='basic'
     )
@@ -42,57 +42,38 @@ def get_arguments():
     parser.add_argument(
         'admin-username',
         metavar='ADMINUSERNAME',
-        required=True,
         help='Username for admin user)'
     )
 
     parser.add_argument(
         'admin-pass',
         metavar='ADMINPASS',
-        required=True,
         help='Password for admin user'
     )
 
-    parser.add_argument(
-        '--database',
-        metavar='DATABASE',
-        help='Database in form localhost:80',
-        default='localhost:27017'
-    )
-
-    parser.add_argument(
-        '--collection',
-        metavar='COLLECTION',
-        help='DB Collection in form db.collection',
-        default='gdas/accounts'
-    )
-
-    return parser.parse_args()
+    return vars(parser.parse_args())
 
 
 def main():
     args = get_arguments()
 
     username = args['user_name']
-    password = hashlib.sha1(args['password'])
-    description = args['user_descr']
+    password = hashlib.sha1(args['password']).hexdigest() +\
+               hashlib.sha1(username).hexdigest()
+    description = args['user-descr']
     user_roles = set(args['user_roles'])
 
     user = {
-        'username': username,
+        'username' : username,
         'password': password,
-        'descr': description,
-        'roles': user_roles
+        'description': description,
+        'roles': list(user_roles)
     }
 
-    auth = UserAuth()
-    if not auth.is_admin(args['admin_username'], args['admin_pass']):
-        raise Exception('Only admin can add users')
-
-    url = args['database'].split(':')
+    url = DATABASE.split(':')
     db = Fatty(url)
-    db.open(args['collection'])
-
+    database, collection = COLLECTION.split('-')
+    db.open(database, collection)
     db.write(user)
 
 if __name__ == '__main__':
