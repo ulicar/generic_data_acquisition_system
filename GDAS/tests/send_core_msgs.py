7aae1f8adf510f1f1150dc736cdd14a40082904c
Josip Domsic <josip.domsic@gmail.com>
2015-05-03 21:26:41 +0200

    *  different test user

--- GDAS/tests/send_core_msgs.py
+++ GDAS/tests/send_core_msgs.py
@@ -3,9 +3,7 @@ __author__ = 'jdomsic'
 import sys
 import requests
 
-
-
 data = sys.stdin.read()
 
 payload = data
-r = requests.request('POST', "http://admin:admin@127.0.0.1:5000/wizard/upload", data=payload)
+r = requests.request('POST', "http://jdomsic:jdomsic@127.0.0.1:5000/wizard/upload", data=payload)
