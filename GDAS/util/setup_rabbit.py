a46a041136cdfe1188a2cf7a4b6584515fd0e4f8
Josip Domsic <josip.domsic@gmail.com>
2015-05-05 23:33:06 +0200

    * different order

--- GDAS/util/setup_rabbit.py
+++ GDAS/util/setup_rabbit.py
@@ -1,6 +1,10 @@
 __author__ = 'jdomsic'
 
+
 """
+    Dead letter exchange are for messages with errors or wrongfully sent msgs.
+    A message in that queue can last only 24h (in 84 600 000 ms)
+
     Creates DURABLE and DIRECT exchanges from EXCHANGES.
 
     Creates queues from QUEUES. Whore string is a queue name, but right part
@@ -8,12 +12,15 @@ __author__ = 'jdomsic'
 
     Creates bindings between QUEUES and EXCHANGES. (q - queue, e - exchange)
 
-    Dead letter exchange are for messages with errors or wrongfully sent msgs.
-    A message in that queue can last only 24h (in 84 600 000 ms)
 
 """
 
 import pika
+
+
+DEADLETTER = {
+    'qDead': 'eDead'
+}
 
 EXCHANGES = ['ePrimary', 'eSecondary']
 
@@ -31,25 +38,10 @@ BINDINGS = {
     'qSecondary.cpu': 'eSecondary',
 }
 
-DEADLETTER = {
-    'qDead': 'eDead'
-}
-
 URL = 'localhost'
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=URL))
 channel = connection.channel()
-for ex in EXCHANGES:
-    channel.exchange_declare(exchange=ex, type='direct', durable=True)
-
-for qu in QUEUES:
-    channel.queue_declare(queue=qu, durable=True)
-
-for qu, ex in BINDINGS.items():
-    routing_key = qu.split('.')[1]
-    channel.queue_bind(exchange=ex, queue=qu, routing_key=routing_key, arguments={
-        'x-dead-letter-exchange': "eDead"
-    })
 
 for q, e in DEADLETTER.items():
     channel.exchange_declare(exchange=e, type='fanout', durable=True)
@@ -57,5 +49,17 @@ for q, e in DEADLETTER.items():
         'x-message-ttl': 24 * 60 * 60 * 1000,
         'x-dead-letter-exchange': "eDead"
     })
+
+for ex in EXCHANGES:
+    channel.exchange_declare(exchange=ex, type='direct', durable=True)
+
+for qu in QUEUES:
+    channel.queue_declare(queue=qu, durable=True, arguments={
+        'x-dead-letter-exchange': "eDead"
+    })
+
+for qu, ex in BINDINGS.items():
+    routing_key = qu.split('.')[1]
+    channel.queue_bind(exchange=ex, queue=qu, routing_key=routing_key)
 
 connection.close()
