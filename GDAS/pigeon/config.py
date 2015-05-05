468ca6413305ec3fdd97fc1a0e252567f3535423
Josip Domsic <josip.domsic@gmail.com>
2015-05-05 22:46:36 +0200

    * removed database

--- GDAS/pigeon/config.py
+++ GDAS/pigeon/config.py
@@ -6,7 +6,6 @@ import logging
 
 class Configuration():
     def __init__(self):
-        self.database = None
         self.mq_url = None
         self.log_file = None
         self.output_exchange = None
@@ -55,7 +54,6 @@ class Configuration():
         self.mq_url = config.get('gdas', 'mq_url')
         self.input_queue = config.get('gdas', 'input_mq').split(':')[1]
         self.output_exchange, self.routing_key = config.get('gdas', 'output_mq').split(':')
-        self.database = config.get('gdas', 'database').split(':')
-        self.name = config.get('GDAS', 'name')
+        self.name = config.get('gdas', 'name')
 
         return self
