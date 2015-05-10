5713aea448544f01b1bc59b935466ea83fcd7969
Josip Domsic <josip.domsic@gmail.com>
2015-05-10 21:15:28 +0200

    * testing cores setup.py

--- GDAS/tests/mocking/cores/setup.py
+++ GDAS/tests/mocking/cores/setup.py
@@ -2,24 +2,34 @@ __author__ = 'jdomsic'
 
 from setuptools import setup
 
-setup(name='GDAS Core',
+setup(name='GDAS test cores',
       version='1.0',
-      description='GDAS testing core',
+      description='GDAS testing cores for Wizard',
       author='Josip Domsic',
       author_email='josip.domsic@gmail.com',
       license='MIT',
       url='https://github.com/ulicar/generic_data_acquisition_system',
       data_files=[
-          ('/opt/gdas/core', [
+          ('/opt/gdas/test/cores', [
+              '__init__.py'
               'simpleCore.py',
+              'simpleActiveCore.py',
               'simplePassiveCore.py',
-              'simpleActiveCore.py'
           ]),
-          ('/etc/gdas/core', [
+          ('/opt/gdas/test/cores/sensorNodes', [
+              '__init__.py'
+              'cpuNode.py',
+              'humidityNode.py',
+              'lightNode.py',
+              'sensorNode.py',
+              'temperatureNode.py'
+          ]),
+          ('/etc/gdas/test/cores', [
+              'simplePassiveCore.ini.default',
               'simplePassiveCore.uwsgi.ini.default',
               'simplePassiveCore.nginx.ini.default'
           ])
       ],
       install_requires=[
-          'flask==0.10'
+          'flask==0.10.1'
       ])
