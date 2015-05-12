9f1f4badb66aa5faa427883c64f9281f47156784
Josip Domsic <josip.domsic@gmail.com>
2015-05-12 15:01:24 +0200

    *  import only class from module

--- GDAS/tests/mocking/cores/coreCreator.py
+++ GDAS/tests/mocking/cores/coreCreator.py
@@ -1,10 +1,12 @@
 __author__ = 'jdomsic'
 
 from simpleCore import Core
-from sensorNodes import cpuNode
-from sensorNodes import humidityNode
-from sensorNodes import lightNode
-from sensorNodes import temperatureNode
+from sensorNodes.cpuNode import CpuNode
+from sensorNodes.counterNode import CounterNode
+from sensorNodes.doorNode import DoorNode
+from sensorNodes.humidityNode import HumidityNode
+from sensorNodes.lightNode import LightNode
+from sensorNodes.temperatureNode import TemperatureNode
 
 
 def create_cores(parser):
@@ -22,10 +24,12 @@ def create_sensors(parser, name):
     core_sensors = list()
     for sensor_type in parser.get(name, 'sensors').strip().split(','):
         sensor_map = {
-            'temperature': temperatureNode.TemperatureNode,
-            'light': lightNode.LightNode,
-            'cpu': cpuNode.CpuNode,
-            'humidity': humidityNode.HumidityNode
+            'temperature': TemperatureNode,
+            'light': LightNode,
+            'cpu': CpuNode,
+            'humidity': HumidityNode,
+            'counter': CounterNode,
+            'door': DoorNode
         }[sensor_type]
 
         core_sensors.extend(get_sensors_by_type(parser, name, sensor_type, sensor_map))
