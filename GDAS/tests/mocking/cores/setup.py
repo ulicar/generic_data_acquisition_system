74520405ff08b0ebc6c819a03f931aa5aca646fe
Josip Domsic <josip.domsic@gmail.com>
2015-05-12 15:02:10 +0200

    *  include new core source

--- GDAS/tests/mocking/cores/setup.py
+++ GDAS/tests/mocking/cores/setup.py
@@ -12,19 +12,23 @@ setup(name='GDAS test cores',
       data_files=[
           ('/opt/gdas/test/cores', [
               '__init__.py',
+              'coreCreator.py',
               'simpleCore.py',
               'simpleActiveCore.py',
               'simplePassiveCore.py',
           ]),
           ('/opt/gdas/test/cores/sensorNodes', [
               'sensorNodes/__init__.py',
+              'counterNode.py',
               'sensorNodes/cpuNode.py',
+              'sensorNodes/doorNode.py',
               'sensorNodes/humidityNode.py',
               'sensorNodes/lightNode.py',
               'sensorNodes/sensorNode.py',
               'sensorNodes/temperatureNode.py'
           ]),
           ('/etc/gdas/test/cores', [
+              'simpleActiveCore.ini.default',
               'simplePassiveCore.ini.default',
               'simplePassiveCore.uwsgi.ini.default',
               'simplePassiveCore.nginx.ini.default'
