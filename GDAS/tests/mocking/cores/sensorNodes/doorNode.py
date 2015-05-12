1c78e781354f138988769e899641e276ab2233a4
Josip Domsic <josip.domsic@gmail.com>
2015-05-12 02:09:49 +0200

    * nodes, nodes, nodes

---
+++ GDAS/tests/mocking/cores/sensorNodes/doorNode.py
@@ -0,0 +1,35 @@
+__author__ = 'jdomsic'
+
+"""
+    Mocks door sensor (open / closed).
+    
+    Prints out door sensor data (changed) every second.
+"""
+
+import json
+import time
+
+from random import random
+
+from sensorNode import SensorNode
+
+
+class DoorNone(SensorNode):
+    def __init__(self, unique_id, module_type, value, timestamp):
+        super(self.__class__, self).\
+            __init__(unique_id, module_type, value, timestamp)
+
+    def update(self):
+        self.update_value(int(time.time()), random() > 0.5)
+
+
+if __name__ == '__main__':
+    DUMMY = 0
+    sensor = DoorNone('Doors01', 'door', DUMMY, DUMMY)
+    sensor.validate()
+
+    while True:
+        sensor.update()
+
+        print json.dumps(sensor.get())
+        time.sleep(0.99)
