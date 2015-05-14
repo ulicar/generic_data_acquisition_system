79a7d2bdcb46cf2d01c931a1d3a2dac1c2600dad
Josip Domsic <josip.domsic@gmail.com>
2015-05-14 03:53:58 +0200

    * build script

---
+++ GDAS/santa/setup.py
@@ -0,0 +1,27 @@
+__author__ = 'jdomsic'
+
+from setuptools import setup
+
+setup(name='GDAS Santa',
+      version='1.0',
+      description='GDAS Santa database interface',
+      author='Josip Domsic',
+      author_email='josip.domsic@gmail.com',
+      license='MIT',
+      url='https://github.com/ulicar/generic_data_acquisition_system',
+      data_files=[
+          ('/opt/gdas/santa', [
+              'santa.py'
+          ]),
+          ('/etc/gdas/wizard', [
+              'santa.ini.default',
+              'santa.conf',
+              'santa.uwsgi.ini.default',
+              'santa.nginx.ini.default',
+              'README.txt'
+          ])
+      ],
+      install_requires=[
+          'GDAS',
+          'flask==0.10.1'
+      ])
