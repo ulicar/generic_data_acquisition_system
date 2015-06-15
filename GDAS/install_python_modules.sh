880afa0584ac7589a4dc2f5516455d8cddd54994
Josip Domsic <josip.domsic@gmail.com>
2015-06-15 21:43:16 +0200

    * added mock dependency

--- GDAS/install_python_modules.sh
+++ GDAS/install_python_modules.sh
@@ -6,5 +6,5 @@ if [ ! $USER == 'root' ]; then
     echo 'Must be superuser' && exit -1
 fi
 
-declare -a PACKAGES=('setuptools' 'flask==0.10.1' 'uwsgi' 'validictory' 'pymongo==3.0' 'pika==0.9.14' 'requests')
+declare -a PACKAGES=('setuptools' 'flask==0.10.1' 'uwsgi' 'validictory' 'pymongo==3.0' 'pika==0.9.14' 'requests' 'mock')
 pip install ${PACKAGES[@]}
