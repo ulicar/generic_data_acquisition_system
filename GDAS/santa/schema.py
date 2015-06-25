ebae89340a061ce97288aa9698e1584ce8a508f0
Josip Domsic <josip.domsic@gmail.com>
2015-06-25 18:21:34 +0200

    * repaired schema

--- GDAS/santa/schema.py
+++ GDAS/santa/schema.py
@@ -6,26 +6,38 @@ from timemodule import to_datetime
 def create_post_data_scheme():
     """ Message created by a single Core whose name is in 'core' """
     schema = {
-        'type': 'object',
-        'properties': {
-            'user':  {'type: string'},
-            'cores': {
-                'type': 'array',
-                'items': 'string'
-            },
-            'modules':  {
-                'type': 'array',
-                'items': 'string'
-            },
-            'time': {
-                'type': 'object',
-                'properties': {
-                    'from': {'type': 'string'},
-                    'to': {'type': 'string'}
-                }
+        "type": "object",
+        "properties": {
+        "user": {
+            "type": "string"
+        },
+        "modules": {
+            "type": "array",
+            "items": {
+                "type": "string"
             }
         },
-        'required': ['user', 'cores', 'modules', 'time']
+        "core": {
+            "type": "string",
+        },
+        "time": {
+          "type": "object",
+          "properties": {
+            "from": {
+              "type": "string"
+            },
+            "to": {
+              "type": "string"
+            }
+          }
+        }
+      },
+      "required": [
+        "user",
+        "modules",
+        "core",
+        "time"
+      ]
     }
 
     return schema
@@ -33,14 +45,14 @@ def create_post_data_scheme():
 
 def map_keys(post_data):
     database = post_data['user']
-    collections = post_data['cores'][:]
+    collections = post_data['core']
     modules = post_data['modules'][:]
 
     try:
         time_start = to_datetime(post_data['time']['from'])
         time_end = to_datetime(post_data['time']['to'])
 
-    except (ValueError, TypeError):
+    except (ValueError, TypeError) as e:
         raise TypeError('Error in field: time')
 
     return database, collections, modules, time_start, time_end
